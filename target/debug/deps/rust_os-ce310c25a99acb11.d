42179b54607b4a0dd85e10ee4bdd0948e01a88e3
Shiven Shekar <shivenshekar01@gmail.com>
2025-01-29 18:00:47 -0700

    Added linker point

---
+++ target/debug/deps/rust_os-ce310c25a99acb11.d
@@ -0,0 +1,5 @@
+/Users/shivenshekar/RustroverProjects/rust-os/target/debug/deps/rust_os-ce310c25a99acb11: src/main.rs
+
+/Users/shivenshekar/RustroverProjects/rust-os/target/debug/deps/rust_os-ce310c25a99acb11.d: src/main.rs
+
+src/main.rs:
