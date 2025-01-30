accbeea8a76fb26a0ee6514c6c4f53a995b4b655
Shiven Shekar <shivenshekar01@gmail.com>
2025-01-29 17:36:46 -0700

    Initial commit

---
+++ target/debug/deps/rust_os-1ecb8c4f30b3d6c9.d
@@ -0,0 +1,5 @@
+/Users/shivenshekar/RustroverProjects/rust-os/target/debug/deps/rust_os-1ecb8c4f30b3d6c9: src/main.rs
+
+/Users/shivenshekar/RustroverProjects/rust-os/target/debug/deps/rust_os-1ecb8c4f30b3d6c9.d: src/main.rs
+
+src/main.rs:
